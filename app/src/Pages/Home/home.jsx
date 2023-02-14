68422ae012448bbe4e42cb86fe534309ec4c70c0
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-14 20:35:07 +0100

    add Header component in Home page

--- app/src/Pages/Home/home.jsx
+++ app/src/Pages/Home/home.jsx
@@ -1,7 +1,12 @@
+import Header from '../../Components/Header/Header'
 const Home = () => {
+    let infoPersonal = [
+        {name:'Younes' , job:'Developer',description:'im Fullstack developer'},
+    ]
     return (
         <>
             <h1>Home page</h1>
+            <Header name={infoPersonal[0].name} job={infoPersonal[0].job}  description={infoPersonal[0].description} />
             {/*  */}
             
             {/*  */}
