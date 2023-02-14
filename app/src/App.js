bd17a15444b55a6f89c4bbb28072e695c5e8ab9c
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-14 15:58:42 +0100

    add Navbar in App

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom" ;
 import './App.css';
 import Routing from './routing';
+import Navbar from './Components/Navbar/Navbar'
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
+     <Navbar />
         <Routing />
         </BrowserRouter>
     </div>
