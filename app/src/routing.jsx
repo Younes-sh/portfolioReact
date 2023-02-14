6f36910100f5307b8c0c543585ff97e8af2f2b0c
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-14 13:25:23 +0100

    edit routing

--- app/src/routing.jsx
+++ app/src/routing.jsx
@@ -1,9 +1,14 @@
-import React from 'react-router-dom'
-
+import { Route, Routes } from "react-router-dom";
+import Home from './Pages/home'
 const Routing = () => {
     return (
         <>
-        
+            <Routes>
+           
+                <Route exact path="/" element={<Home />} />
+           
+            </Routes>
+
         </>
     )
 }
