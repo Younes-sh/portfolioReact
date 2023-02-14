43adc800e33915049955c2beb4111f8ea66db77d
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-14 16:42:04 +0100

    add address contact in routing

--- app/src/routing.jsx
+++ app/src/routing.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './Pages/Home/home'
+import Contact from "./Pages/Contact/contact";
 const Routing = () => {
     return (
         <>
             <Routes>
            
                 <Route exact path="/" element={<Home />} />
+                <Route exact path="/contact" element={<Contact />}/>
            
             </Routes>
 
