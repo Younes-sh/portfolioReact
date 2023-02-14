4bd957e8a39b070308af6668f2a91cde4a28db85
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-14 13:18:34 +0100

    add link pages

--- app/src/Components/Navbar/Navbar.jsx
+++ app/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='Navbar'>
+        <div className='container-menu'>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+                <li>
+                    <Link to='/projects'>Projects</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
