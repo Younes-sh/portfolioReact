8c234edc4905f57a9b3bdd4ce69a6ee070127182
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-14 17:51:19 +0100

    add link Projects page

--- app/src/routing.jsx
+++ app/src/routing.jsx
@@ -1,16 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './Pages/Home/home'
 import Contact from "./Pages/Contact/contact";
+import Projects from "./Pages/Projects/projects";
 const Routing = () => {
     return (
         <>
             <Routes>
-           
-                <Route exact path="/" element={<Home />} />
+                <Route exact path="/" element={<Home />}/>
                 <Route exact path="/contact" element={<Contact />}/>
-           
+                <Route exact path="/projects" element={<Projects />} />
             </Routes>
-
         </>
     )
 }
