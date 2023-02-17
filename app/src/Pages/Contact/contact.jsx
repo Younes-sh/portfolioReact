63ba59e02ed7c2eec48f2ca129b1013fd20850a3
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-17 23:31:44 +0100

    styled contact page

--- app/src/Pages/Contact/contact.jsx
+++ app/src/Pages/Contact/contact.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import './contact.css'
 const Contact = () => {
   return (
     <div className='contact'>
+        <h1>Younes</h1>
+        <h1>Contact</h1>
+        <h1>Contact</h1>
+        <h1>Contact</h1>
+        <h1>Contact</h1>
+        <h1>Contact</h1>
+        <h1>Contact</h1>
+        <h1>Contact</h1>
+        <h1>Contact</h1>
         <h1>Contact</h1>
     </div>
   )
