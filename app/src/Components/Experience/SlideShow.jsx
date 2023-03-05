617c800a1141214c860b55e47c9517ce20757b79
Younes-sh <sheikhlaryounes@gmail.com>
2023-03-05 20:09:45 +0100

    create code slide show in slide-show component

--- app/src/Components/Experience/SlideShow.jsx
+++ app/src/Components/Experience/SlideShow.jsx
@@ -3,16 +3,41 @@ import image1 from '../../assets/Imge/coffee.png'
 import image2 from '../../assets/Imge/crypto.png'
 import image3 from '../../assets/Imge/panel.png'
 import image4 from '../../assets/Imge/younicoin.png'
-const Experiences = () => {
+
+
+
+const proprietes = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true
+}
+
+const SlideShow = () => {
   return (
-	<div>
+	<div className='containerSlide'>
 
-
-		
-
+    <Slide {...proprietes}>
+        <div className="each-slide">
+            <div>
+                <img src={img1} alt="img1" />
+            </div>
+        </div>
+        <div className="each-slide">
+            <div>
+                <img src={img2} alt="img2" />
+            </div>
+        </div>
+        <div className="each-slide">
+            <div>
+                <img src={img3} alt="img3" />
+            </div>
+        </div>
+    </Slide>
 
 	</div>
   )
 }
 
-export default Experiences
+export default SlideShow
