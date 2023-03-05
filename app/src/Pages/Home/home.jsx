c2fc19f9edc7d8190830743ac6b77e4e28d9d319
Younes-sh <sheikhlaryounes@gmail.com>
2023-03-05 20:05:57 +0100

    edit address slide-show in home page

--- app/src/Pages/Home/home.jsx
+++ app/src/Pages/Home/home.jsx
@@ -1,6 +1,6 @@
 import Header from '../../Components/Header/Header';
 import './home.css'
-import  Experiences  from '../../Components/Experience/Experiences';
+import  Experiences  from '../../Components/Experience/SlideShow';
 const Home = () => {
     let infoPersonal = [
         {name:'Younes' , job:'Developer',description:`i'm Fullstack developer`},
