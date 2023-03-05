c879cfa6153e39e48200d7c6a084abc70d617afe
Younes-sh <sheikhlaryounes@gmail.com>
2023-03-05 20:01:41 +0100

    remove slide-show code in Experiences

--- app/src/Components/Experience/Experiences.jsx
+++ app/src/Components/Experience/Experiences.jsx
@@ -8,25 +8,7 @@ const Experiences = () => {
 	<div>
 
 
-		<article class="slide">
-		  <div class="slide-show">
-		    <div class="slide-item"> <img class="slide-img" src={image1} /></div>
-		    <div class="slide-item"> <img class="slide-img" src={image2} /></div>
-		    <div class="slide-item"> <img class="slide-img" src={image3} /></div>
-		    <div class="slide-item"> <img class="slide-img" src={image4} /></div>
-		  </div>
-		  <div class="slide-text">
-		    <div class="half a-center">
-		      <h2 class="alide-title">"The slide" just watch it</h2>
-		    </div>
-		    <div class="half a-down"><a class="btn over-transparent" href="">Button Here</a></div>
-		    <ul class="slide-indicator">
-		      <li class="indicator-item"><a rol="low-indicator"></a></li>
-		      <li class="indicator-item"><a rol="low-indicator"></a></li>
-		      <li class="indicator-item"><a rol="low-indicator"></a></li>
-		    </ul>
-		  </div>
-		</article>
+		
 
 
 	</div>
